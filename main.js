9377e790c4a2a0ffdb8d4c790467b342d0da77df
jc020207 <54868777+jc020207@users.noreply.github.com>
2020-05-07 21:02:10 -0400

    framework

---
+++ main.js
@@ -0,0 +1,105 @@
+/* =====================
+Leaflet Configuration
+===================== */
+
+var map = L.map('map', {
+  center: [37.753267, -122.514989],
+  zoom: 12
+});
+
+
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
+
+/* =====================
+Specify Data
+===================== */
+
+var hexmap = "https://raw.githubusercontent.com/jc020207/Final_Project/master/hexmap.geojson";
+var infrustructure = "https://raw.githubusercontent.com/jc020207/Final_Project/master/Infrastructure.geojson";
+var communityC = "https://raw.githubusercontent.com/jc020207/Final_Project/master/CommunityC.geojson";
+var featureGroup;
+
+/* =====================
+Custom symbols
+===================== */
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+
+/* =====================
+Load Initial Slide
+===================== */
+$(document).ready(function() {
+  $.ajax(communityC).done(function(data) {
+    var parsedData = JSON.parse(data);
+    featureGroup = L.geoJson(parsedData, {
+      pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+      }).addTo(map);
+    // quite similar to _.each
+    //featureGroup.eachLayer(eachFeatureFunction);
+  });
+});
+
+/* =====================
+Load Initial Slide
+===================== */
+// load hexagon layer
+var loadhex = function(slide) {
+  //remove previous layer when load
+  map.removeLayer(featureGroup);
+  //load slides
+  $(document).ready(function() {
+    $.ajax(hexmap).done(function(data) {
+      var parsedData = JSON.parse(data);
+      featureGroup = L.geoJson(parsedData, {
+    }).addTo(map);
+    });
+  });};
+
+// load infrastructure and community Center Layer
+  var loadinfra = function(slide) {
+      //remove previous layer when load
+      map.removeLayer(featureGroup);
+      //load slides
+      $(document).ready(function() {
+        $.ajax(infrustructure).done(function(data) {
+          var parsedData = JSON.parse(data);
+          featureGroup = L.geoJson(parsedData, {
+        }).addTo(map);
+        });
+      });
+
+      $(document).ready(function() {
+        $.ajax(communityC).done(function(data) {
+          var parsedData = JSON.parse(data);
+          featureGroup = L.geoJson(parsedData, {
+        }).addTo(map);
+        });
+      });
+    };
+
+// Click Button
+$('#activeness').click(function(e) {
+          loadhex();
+        });
+
+$('#suitability').click(function(e) {
+          loadhex();
+        });
+
+$('#infrastructure').click(function(e) {
+          loadinfra();
+        });
