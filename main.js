e00eabe4445303527f3e3d7c37923ddc30fb5db6
jc020207 <54868777+jc020207@users.noreply.github.com>
2020-05-09 19:07:30 -0400

    update

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var map = L.map('map', {
   zoom: 12
 });
 
-
 var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
@@ -26,7 +25,7 @@ var infra;
 var center;
 
 /* =====================
-Custom symbols
+Custom symbols and styles
 ===================== */
 
 var geojsonMarkerOptions = {
@@ -38,54 +37,225 @@ var geojsonMarkerOptions = {
     fillOpacity: 0.8
 };
 
+var introstyle = function(feature) {
+    return {
+        fillColor: '#ffffff',
+        weight: 0.5,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.5
+    };
+}
+
+//style for activeness map
+var activecolor =function(d) {
+    return d > 5.52   ? '#800026' :
+           d > 4.92   ? '#bd0026' :
+           d > 4.33   ? '#e31a1c' :
+           d > 3.74   ? '#fc4e2a' :
+           d > 3.14   ? '#fd8d3c' :
+           d > 2.55   ? '#feb24c':
+           d > 1.96   ? '#fed976' :
+           d > 1.37   ? '#ffeda0' :
+           d > 0.77   ? '#ffffcc' :
+                      '#FCF8EC';
+}
+
+var activestyle = function(feature) {
+    return {
+        fillColor: activecolor(feature.properties.FINAL),
+        weight: 0.5,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.7
+    };
+}
+
+//style for suitability map
+var suitcolor =function(d) {
+    return d > 5.57   ? '#084081' :
+           d > 3.09   ? '#0868ac' :
+           d > 2.07   ? '#2b8cbe' :
+           d > 1.36   ? '#4eb3d3' :
+           d > 0.98   ? '#7bccc4' :
+           d > 0.69   ? '#a8ddb5':
+           d > 0.49   ? '#ccebc5' :
+           d > 0.32   ? '#e0f3db' :
+           d > 0.14   ? '#f7fcf0' :
+                      'F9FBF6';
+}
+
+var suitstyle = function(feature) {
+    return {
+        fillColor: suitcolor(feature.properties.FINAL),
+        weight: 0.5,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.7
+    };
+}
+
 
 /* =====================
 Load Initial Slide
 ===================== */
-$(document).ready(function() {
-  $.ajax(communityC).done(function(data) {
-    var parsedData = JSON.parse(data);
-    featureGroup = L.geoJson(parsedData, {
-      pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng, geojsonMarkerOptions);
-    },
-      }).addTo(map);
-    // quite similar to _.each
-    //featureGroup.eachLayer(eachFeatureFunction);
-  });
-});
 
-/* =====================
-Load Slide Function
-===================== */
-// load activeness layer
-var loadact = function(slide) {
-  //remove previous layer when load
-  map.removeLayer(featureGroup);
-//  map.removeLayer(featureGroup1);
-  //load slides
   $(document).ready(function() {
     $.ajax(hexmap).done(function(data) {
       var parsedData = JSON.parse(data);
       featureGroup = L.geoJson(parsedData, {
+      style:introstyle
     }).addTo(map);
     });
-  });};
+  });
 
-// load suitabiltiy layer
-  var loadsuit = function(slide) {
+
+/* =====================
+  click to show radar chart event for real estate activeness map
+    ===================== */
+// set up chart
+
+var activechartdata ={
+labels: ['Social Activeness', 'Commercial Activeness', 'Transportation Accessibiltiy', 'Development Demand'],
+datasets: [{
+  label:'index',
+  backgroundColor:['#DAF7A6','#FFC300','#FF5733','#C70039'],
+  data: [0, 0, 0, 0]
+}]
+}
+
+var options = {
+  scale: {
+    ticks: {
+      beginAtZero: true,
+      min: 0,
+      max: 10,
+      stepSize: 5
+    }
+  },
+  legend: {
+    position: 'left',
+    labels: {  "fontSize": 10,}
+  },
+  tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    var label = data.datasets[tooltipItem.datasetIndex].label || '';
+                    if (label) {
+                        label += ': ';
+                    }
+                    label += Math.round(tooltipItem.yLabel * 100) / 100;
+                    return label;
+                }}}
+};
+
+var myactivechart = document.getElementById('myactiveChart').getContext('2d');
+
+//plot chart function
+var plotactiveradar = function(data){ myactiveRadarChart = new Chart(myactivechart, {
+type: 'polarArea',
+data: data,
+options: options
+});}
+
+//function to get data and plot chart
+var getactivechartdata = function(layer) {
+layer.on('click', function (event) {
+activechartdata.datasets[0].data[0]=layer.feature.properties.social;
+activechartdata.datasets[0].data[1]=layer.feature.properties.act_final;
+activechartdata.datasets[0].data[2]=layer.feature.properties.trans_nrom;
+activechartdata.datasets[0].data[3]=layer.feature.properties.dem_norm;
+console.log(activechartdata);
+plotactiveradar(activechartdata);
+});
+};
+
+/* =====================
+  click to show radar chart event for suitability map
+    ===================== */
+// set up chart
+
+var suitchartdata ={
+labels: ['Contamination', 'Land Use', 'Flood Plain', 'Slope'],
+datasets: [{
+  label:'index',
+  backgroundColor:['#DAF7A6','#FFC300','#FF5733','#C70039'],
+  data: [0, 0, 0, 0]
+}]
+}
+
+var mysuitchart = document.getElementById('mysuitChart').getContext('2d');
+
+//plot chart function
+var plotsuitradar = function(data){ mysuitRadarChart = new Chart(mysuitchart, {
+type: 'polarArea',
+data: data,
+options: options
+});}
+
+//function to get data and plot chart
+var getsuitchartdata = function(layer) {
+layer.on('click', function (event) {
+suitchartdata.datasets[0].data[0]=layer.feature.properties.cont_norm;
+suitchartdata.datasets[0].data[1]=layer.feature.properties.land_norm;
+suitchartdata.datasets[0].data[2]=layer.feature.properties.flood_norm;
+suitchartdata.datasets[0].data[3]=layer.feature.properties.slope_norm;
+console.log(suitchartdata);
+plotsuitradar(suitchartdata);
+});
+};
+
+
+/* =====================
+Load Slide Function
+===================== */
+// load intro layer
+  var loadintro = function(slide) {
     //remove previous layer when load
     map.removeLayer(featureGroup);
-  //  map.removeLayer(featureGroup1);
     //load slides
     $(document).ready(function() {
       $.ajax(hexmap).done(function(data) {
         var parsedData = JSON.parse(data);
         featureGroup = L.geoJson(parsedData, {
+        style:introstyle
       }).addTo(map);
+      });});
+};
+
+
+// load activeness layer
+var loadact = function(slide) {
+  //remove previous layer when load
+  map.removeLayer(featureGroup);
+  //load slides
+  $(document).ready(function() {
+    $.ajax(hexmap).done(function(data) {
+      var parsedData = JSON.parse(data);
+      featureGroup = L.geoJson(parsedData, {
+      style: activestyle
+    }).addTo(map);
+    featureGroup.eachLayer(getactivechartdata);
+    });
+  });};
+
+
+// load suitabiltiy layer
+  var loadsuit = function(slide) {
+    //remove previous layer when load
+    map.removeLayer(featureGroup);
+    //load slides
+    $(document).ready(function() {
+      $.ajax(hexmap).done(function(data) {
+        var parsedData = JSON.parse(data);
+        featureGroup = L.geoJson(parsedData, {
+        style:suitstyle
+      }).addTo(map);
+      featureGroup.eachLayer(getsuitchartdata);
       });
     });};
 
+//load both infrasturcture and community Center
 var loadboth = function(x){$(document).ready(function() {
   $.ajax(infrustructure).done(function(data) {
     var parsedData = JSON.parse(data);
@@ -103,26 +273,209 @@ $(document).ready(function() {
   });
 });}
 
+// run the function to load both layers
 loadboth();
 
-// load infrastructure and community Center Layer
+// map both infrastructure and community center
   var loadinfra = function(slide) {
       //remove previous layer when load
       map.removeLayer(featureGroup);
       //load slides
-
       featureGroup=L.layerGroup([infra, center]).addTo(map);
     };
 
-// Click Button
+
+
+  /* =====================
+    build real estate develiopment activeness slider
+      ===================== */
+// setup slider
+    var activeslider = document.getElementById('activeslider');
+
+    noUiSlider.create(activeslider, {
+        start: [0, 6.1],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 6.1},
+        tooltips:[true,true]
+    });
+// define global variables
+var activeindex
+var activeslidemin
+var activeslidemax
+
+//slider move event
+    activeslider.noUiSlider.on('change',function(){
+    activeindex = activeslider.noUiSlider.get();
+    activeslidemin = activeindex[0];
+    activeslidemax = activeindex[1];
+    loadactslide();
+    })
+
+// slider filter funciton
+var activeslidefilter = function(feature){
+  return (feature.properties.FINAL<=activeslidemax && feature.properties.FINAL>=activeslidemin)
+}
+
+// load activeness layer function
+    var loadactslide = function(slide) {
+      //remove previous layer when load
+      map.removeLayer(featureGroup);
+      //load slides
+      $(document).ready(function() {
+        $.ajax(hexmap).done(function(data) {
+          var parsedData = JSON.parse(data);
+          featureGroup = L.geoJson(parsedData, {
+          style: activestyle,
+          filter: activeslidefilter
+        }).addTo(map);
+        featureGroup.eachLayer(getactivechartdata);
+        });
+      });};
+
+      /* =====================
+        build suitabiltiy slider
+          ===================== */
+
+
+    // setup slider
+        var suitslider = document.getElementById('suitslider');
+
+        noUiSlider.create(suitslider, {
+            start: [0, 6.1],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': 10},
+            tooltips:[true,true]
+        });
+    // define global variables
+    var suitindex
+    var suitslidemin
+    var suitslidemax
+
+    //slider move event
+        suitslider.noUiSlider.on('change',function(){
+        suitindex = suitslider.noUiSlider.get();
+        suitslidemin = suitindex[0];
+        suitslidemax = suitindex[1];
+        loadsuitslide();
+        })
+
+    // slider filter funciton
+    var suitslidefilter = function(feature){
+      return (feature.properties.suit_2<=suitslidemax && feature.properties.suit_2>=suitslidemin)
+    }
+
+    // load activeness layer function
+        var loadsuitslide = function(slide) {
+          //remove previous layer when load
+          map.removeLayer(featureGroup);
+          //load slides
+          $(document).ready(function() {
+            $.ajax(hexmap).done(function(data) {
+              var parsedData = JSON.parse(data);
+              featureGroup = L.geoJson(parsedData, {
+              style: suitstyle,
+              filter: suitslidefilter
+            }).addTo(map);
+            featureGroup.eachLayer(getsuitchartdata);
+            });
+          });};
+
+
+
+    /* =====================
+    Click Button
+    ===================== */
+// Introduction section button
+$('#introduction').click(function(e) {
+              loadintro();
+              $('.sidebar').hide();
+              $('#sidebar_original').show();
+            });
+
 $('#activeness').click(function(e) {
           loadact();
+          $('.sidebar').hide();
+          $('#sidebar_1').show();
         });
 
 $('#suitability').click(function(e) {
           loadsuit();
+          $('.sidebar').hide();
+          $('#sidebar_2').show();
         });
 
 $('#infrastructure').click(function(e) {
           loadinfra();
+          $('.sidebar').hide();
+          $('#sidebar_3').show();
         });
+
+// real-estate activeness section button
+$('#introduction_1').click(function(e) {
+                      loadintro();
+                  $('.sidebar').hide();
+                   $('#sidebar_original').show();
+                    });
+$('#activeness_1').click(function(e) {
+                  loadact();
+                  $('.sidebar').hide();
+                  $('#sidebar_1').show();
+                });
+$('#suitability_1').click(function(e) {
+                  loadsuit();
+                  $('.sidebar').hide();
+                  $('#sidebar_2').show();
+                });
+$('#infrastructure_1').click(function(e) {
+                  loadinfra();
+                  $('.sidebar').hide();
+                  $('#sidebar_3').show();
+                });
+
+// suitability activeness section button
+$('#introduction_2').click(function(e) {
+                  loadintro();
+                  $('.sidebar').hide();
+                  $('#sidebar_original').show();
+                });
+$('#activeness_2').click(function(e) {
+                  loadact();
+                  $('.sidebar').hide();
+                  $('#sidebar_1').show();
+                });
+$('#suitability_2').click(function(e) {
+                  loadsuit();
+                  $('.sidebar').hide();
+                  $('#sidebar_2').show();
+                });
+$('#infrastructure_2').click(function(e) {
+                  loadinfra();
+                  $('.sidebar').hide();
+                  $('#sidebar_3').show();
+                });
+
+// infrustructure section button
+$('#introduction_3').click(function(e) {
+                  loadintro();
+                  $('.sidebar').hide();
+                  $('#sidebar_original').show();
+                });
+$('#activeness_3').click(function(e) {
+                  loadact();
+                  $('.sidebar').hide();
+                  $('#sidebar_1').show();
+                });
+$('#suitability_3').click(function(e) {
+                  loadsuit();
+                  $('.sidebar').hide();
+                  $('#sidebar_2').show();
+                });
+$('#infrastructure_3').click(function(e) {
+                  loadinfra();
+                  $('.sidebar').hide();
+                  $('#sidebar_3').show();
+                });
